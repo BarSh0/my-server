54e2ff5aba083a9e139e84b61a1655775a558c8b
BarSh0 <barshoshani274@gmail.com>
2023-03-31 20:00:44 +0300

    check

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const { mongoConnect } = require('./config/mongoDB');
 const router = require('./router');
 const server = require('http').createServer(router);
 
 require('./services/googleSheets.services');
 
 const PORT = 5000;
-mongoConnect();
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
